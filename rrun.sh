445179e57815418ff8a3d8eb3a108ab712bf7b71
oxrinz <roberts.denisovs@gmail.com>
2026-04-18 18:02:12 +0200

    wip

--- rrun.sh
+++ rrun.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
+if [ -z "$1" ]; then
+  echo "usage: ./rrun.sh <script.py>"
+  exit 1
+fi
+
 git add -A
 git commit -m "wip" --allow-empty
 git push
 
-ssh ryujin 'cd ~/beaml && git pull && python3 '"$1"
+ssh ryujin "cd ~/beaml && git pull && python3 $1"
